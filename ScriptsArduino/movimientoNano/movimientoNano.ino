ca4f3b7e393d17c9d6b29a8c76d8073db6afb659
Pandari <pandari260@gmail.com>
2018-05-13 15:14:19 -0300

    me falto el .ino

--- ScriptsArduino/movimientoNano/movimientoNano.ino
+++ ScriptsArduino/movimientoNano/movimientoNano.ino
@@ -1,25 +1,38 @@
 #include <Servo.h>
-
-Servo servo;
-Servo servo2;
-char grado = 90;
-
-
  
-void setup(){
-  Serial.begin(9600);
-  servo.attach(6);
-  servo2.attach(8);
-  servo2.write(90);
-  servo.write(grado);  
+int val = 0; //Variable de entrada del Serial
+ 
+Servo servoH; //Creamos un objeto Servo de nombre... servo
+Servo servoV;
+Servo servoActual;
+char eje = "";
+void setup()
+{
+   Serial.begin(9600); //Iniciamos el serial
+   servoH.attach(6); //Conectamos el servo al pin digital 3
+   servoV.attach(8); //Conectamos el servo al pin digital 3
+
 }
  
-void loop(){
-  
-  if (Serial.available()>0){
-   grado = Serial.read();
-
+void loop()
+{
+   
+   if(Serial.available() > 0) //Detecta si hay alguna entrada por serial
+   {
+      eje = (char) Serial.read();
+      if( eje == 'y'){
+        servoActual = servoH;
+      }
+      else if (eje == 'x'){
+        servoActual = servoV;
+      }
+      
+      val = Serial.parseInt();
+      if(val != 0)
+      {
+         servoActual.write(val); //Mueve el servo a la posición entrada (excepto si es 0)
+      }
    }
-       servo.write(grado);
-
+   delay(20);
 }
+  
